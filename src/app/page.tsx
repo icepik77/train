0dbb75d747c42f3d8b0337b16ab267eee5442a3b
icepik77 <icepik77@mail.ru>
2024-12-18 13:56:05 +0300

    new color

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -25,7 +25,7 @@ export default function Home() {
         <div className="bg-sky-500 border-4 border-blue-500 w-full h-20"></div>
         <div className="bg-sky-500 border-4 border-blue-500 w-full h-20 relative">
           <div className="bg-violet-500 border-2 border-violet-500 w-40 h-10 absolute top-0 right-0 relative">
-            <div className="absolute bottom-0 right-0 bg-red-600 border-2 border-lime-600 w-7 h-7 translate-y-1/2">
+            <div className="absolute bottom-0 right-0 bg-lime-500 border-2 border-lime-600 w-7 h-7 translate-y-1/2">
 
             </div>
           </div>
